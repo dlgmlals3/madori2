a64f550fc26702250a05e6ec96334dcdd05ed976
simon <dlgmlals3@gmail.com>
2018-05-26 03:42:11 +0900

    node example

--- pages/node/node.ts
+++ pages/node/node.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Injectable } from '@angular/core';
+//import { Http, Response} from '@angular/http';
+import { HttpClient} from '@angular/common/http';
+
 @Component({
   selector: 'page-node',
   templateUrl: 'node.html'
 })
 export class NodePage {
-
+  private http: HttpClient;
   constructor(public navCtrl: NavController) {
-
+  	console.log('Hello Service Provider');
+	//this.http.post("https://218.38.52.30:8282", "555","555");
+        this.http.get('https://218.38.52.30:8282');
   }
-
 }
